5939f80036922e9b27caec16f719743d9c476788
Nyaroeh <florencenyarotso@gmail.com>
2023-02-17 12:35:34 -0800

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in lowercase
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	for (i 'a' : i <= 'z' : i++)
+		putchar(i);
+	putchar("/n");
+	return (0);
+}
