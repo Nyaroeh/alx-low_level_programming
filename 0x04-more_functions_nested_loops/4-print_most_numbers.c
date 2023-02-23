3b81a7d741b357e845d5e280ca53b7d64dde6801
Nyaroeh <florencenyarotso@gmail.com>
2023-02-23 12:13:07 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+		}
+	_putchar('\n');
+}
