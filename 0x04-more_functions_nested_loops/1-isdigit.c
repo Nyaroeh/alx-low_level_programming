d5eacfc607a127d002e6e72ed4b1b4ef28f5d603
Nyaroeh <florencenyarotso@gmail.com>
2023-02-23 12:03:52 -0800

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code
+ *@c: parameter to be measured
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
