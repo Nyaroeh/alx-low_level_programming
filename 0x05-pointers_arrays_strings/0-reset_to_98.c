3aa10affb87c5dac1c6a0e7a64eb3ad3aef72722
Nyaroeh <florencenyarotso@gmail.com>
2023-02-27 11:07:06 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointer as parameter and updates the value
+ * @n: parameter
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
