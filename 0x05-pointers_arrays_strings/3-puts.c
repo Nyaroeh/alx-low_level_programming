2580977b3bab8bc0930e3501330cec1558724d85
Nyaroeh <florencenyarotso@gmail.com>
2023-02-27 11:15:20 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: parameter
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
